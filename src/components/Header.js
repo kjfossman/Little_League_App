fee9861d04cea3b3a64501401ccc56fb59673c91
Kyle <kjfossman@gmail.com>
2021-06-18 16:36:28 -0800

    Added a header component with a logo for and added a span that will be used for text

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Lit_League_Logo from '../Lit-League-Logo.svg';
+import '../css_files/Header.css';
+
+class Header extends React.Component {
+    render() {
+      return (
+        <div className="Header">
+            
+            <img src={Lit_League_Logo} alt="logo" className="logo_left"/>
+            <span>Welcome to the Haines Little League Charter #99827</span>
+            <img src={Lit_League_Logo} alt="logo" className="logo_right"/>
+            
+        </div>
+      );
+    }
+  }
+
+
+export default Header;
