0c4a2aab5b7c4162e60e79b4654da2a09029153f
Kyle <kjfossman@gmail.com>
2021-06-21 10:23:39 -0800

    Schedule compenent is looking better now you can see the game times teams and scores.  Now I have to work on the backend to be able to handle a game that does not yet have a score.

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Moment from 'moment';
+import '../css_files/Schedule.css';
 
 class Schedule extends Component {
+
     render() {
+
+        let games = this.props.games.map(game => 
+            <div className="sched">{Moment(game.date).format('MMMM: DD LT')} <div className='score'>{"SCORE"}</div>
+                <div className='teams'>{game.away_team.name}<div className='team-score'>{game.away_team_score}</div><br></br> 
+                {game.home_team.name}<div className='team-score'>{game.home_team_score}</div></div><br></br></div>)
+
         return (
             <div>
-                {Moment(this.props.games[0].date).format('MMMM: DD LT')}
+                {games}
+                    
+                {/* {Moment(this.props.games[0].date).format('MMMM: DD LT')} */}
             </div>
         );
     }
