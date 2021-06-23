01813f0998f5f9368c4f36b4957e79142ab273fc
Kyle <kjfossman@gmail.com>
2021-06-23 11:00:38 -0800

    Added to playercontainer and player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import '../css_files/Player.css';
 
 class Player extends Component {
     render() {
         return (
-            <div>
-                {this.props.player.name}
+            <div className="flip-card">
+                <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                        <img className='playerimg' src="https://a.espncdn.com/photo/2017/0817/Jedd_Gyorko1.png" alt="picture"></img>
+                    </div>
+                    <div className="flip-card-back">
+                        <h1>{this.props.player.name}</h1>
+                        <p>{this.props.player.age}</p>
+                    </div>
+                </div>
             </div>
         );
     }
