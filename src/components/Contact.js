e9b728875b04e13f5aa74ac139e0dbbc53a2faea
Kyle <kjfossman@gmail.com>
2021-07-14 09:19:16 -0800

    Styled the contacts page to put each contact in their own block with a white background so it is easier to read

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+import '../css_files/Contact.css';
 class Contact extends Component {
     render() {
         return (
-            <div>
-                {this.props.name}
-                {this.props.email}
-                {this.props.phone}
+            <div className="main">
+                {this.props.name} - {this.props.title}<br></br>
+                {this.props.email}<br></br>
+                {this.props.phone}<br></br>
                 
             </div>
         );
--- src/containers/ContactsContainer.js
+++ src/containers/ContactsContainer.js
@@ -8,7 +8,7 @@ class ContactsContainer extends Component {
     
     render() {
 
-        let contacts = this.props.contacts.map(contact => <Contact key={contact.id} name={contact.name} phone={contact.phone} email={contact.email}/>)
+        let contacts = this.props.contacts.map(contact => <Contact key={contact.id} name={contact.name} phone={contact.phone} email={contact.email} title={contact.title}/>)
         console.log(this.props)
         return (
             <div>
